6f1bcca52734d11b029567ed76ab2b87f1cab1fc
PehlivanMert <pehlivannmert@gmail.com>
2025-02-26 17:22:21 +0300

    Fix import path for AuthContext in NotificationManager component

--- src/utils/weather-theme-notify/NotificationManager.jsx
+++ src/utils/weather-theme-notify/NotificationManager.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../../components/contexts/AuthContext";
 import {
   requestNotificationPermission,
   setupForegroundNotifications,
